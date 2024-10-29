da9afd85844f181c73e5454cc7512e2c3886d811
af4oz <0xafz.dev@gmail.com>
2024-10-29 18:53:47 +0530

    Add Header in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 export default function Layout({ children }: any) {
   return (
     <div className="container mx-auto">
+      <header className="my-8 w-full text-center">RatingsMania</header>
       <main>{children}</main>
     </div>
   );
