581a478af4fb2f4cd6233b0833d3ec35fa463040
af4oz <0xafz.dev@gmail.com>
2024-11-05 23:13:30 +0530

    Fix tests

--- tests/rating.spec.ts
+++ tests/rating.spec.ts
@@ -5,67 +5,95 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("New Rating", () => {
-  test("can create a new rating", async ({ page }) => {
-    // 1. can create a empty rating
+  test("can create/edit/fork a rating", async ({ page }) => {
+    // 1. Can create a rating with name "Rating1"
     await page.getByRole("link", { name: "+ New rating", exact: true }).click();
+    await page.getByTestId("criterion-name").fill("Criterion 1");
     await page.getByRole("button", { name: "Calculate" }).click();
+    await expect(page.getByTestId("final-rating")).toContainText(
+      "Final Rating: 5"
+    );
     await page.getByRole("button", { name: "Save" }).click();
+    await page
+      .getByRole("dialog")
+      .getByPlaceholder("Enter a name")
+      .fill("Rating1");
+    await page
+      .getByRole("dialog")
+      .getByRole("button", { name: "Save" })
+      .click();
 
-    // 2. can edit a rating
-    await page.getByRole("link", { name: "Untitled" }).click();
+    // 2. can edit a existing rating "Rating1"
+    await page.getByRole("link", { name: "Rating1" }).click();
+    await page.waitForTimeout(1000); // TODO why timeout needed
     await expect(page.getByRole("heading")).toContainText(
-      "Rating Name: Untitled"
+      "Rating Name: Rating1"
     );
     await page.getByRole("button", { name: "Edit 📝" }).click();
+    // Add a new criterion row
     await page.getByRole("button", { name: "➕" }).click();
     await page
-      .getByRole("row", { name: "Default 0 5 ❌" })
-      .getByRole("textbox")
-      .fill("Default1");
+      .getByRole("cell", { name: "Default" })
+      .getByTestId("criterion-name")
+      .fill("Criterion 2");
     await page
-      .getByRole("row", { name: "Default1 0 5 ❌" })
-      .getByPlaceholder("0")
+      .getByRole("row", { name: "Criterion 2" })
+      .getByTestId("criterion-rating")
+      .fill("7");
+    await page
+      .getByRole("row", { name: "Criterion 2" })
+      .getByTestId("criterion-weight")
       .fill("1");
+    await page.getByRole("button", { name: "Calculate" }).click();
+    await expect(page.getByRole("main")).toContainText(
+      "Error: All weights summed should be equal to 100"
+    );
+    // fix weight error by reducing previous criterion weight by 1
     await page
-      .getByRole("cell", { name: "100" })
-      .getByPlaceholder("0")
-      .fill("98");
+      .getByRole("row", { name: "Criterion 1" })
+      .getByTestId("criterion-weight")
+      .fill("99");
+    await page.getByRole("button", { name: "Calculate" }).click();
+    // TODO check final rating
+    await expect(page.getByTestId("final-rating")).toContainText(
+      "Final Rating: 5.02"
+    );
+    await page.getByRole("button", { name: "Save edit", exact: true }).click();
+
+    // 3. can fork a rating
+    await page.getByRole("link", { name: "Rating1" }).click();
+    await page.getByRole("button", { name: "Fork" }).click();
+    await page.getByRole("button", { name: "➕" }).click();
+    await page
+      .getByRole("row", { name: "Default" })
+      .getByTestId("criterion-name")
+      .fill("Criterion 3");
+    await page
+      .getByRole("row", { name: "Criterion 3" })
+      .getByTestId("criterion-weight")
+      .fill("1");
+
     await page.getByRole("button", { name: "Calculate" }).click();
     await expect(page.getByRole("main")).toContainText(
       "Error: All weights summed should be equal to 100"
     );
     await page
-      .getByRole("cell", { name: "98" })
-      .getByPlaceholder("0")
-      .fill("99");
-    await page.getByRole("button", { name: "Calculate" }).click();
+      .getByRole("row", { name: "Criterion 1" })
+      .getByTestId("criterion-weight")
+      .fill("98");
     // TODO check final rating
+    await page.getByRole("button", { name: "Calculate" }).click();
+    await expect(page.getByTestId("final-rating")).toContainText(
+      "Final Rating: 5.02"
+    );
+    await page.getByRole("button", { name: "Save" }).click();
+    await page.getByPlaceholder("Enter a name").fill("FokedRating1");
     await page.getByRole("button", { name: "Save" }).click();
 
-    // 3. can fork a rating
-    await page.getByRole("link", { name: "Untitled" }).click();
-    await page.getByRole("button", { name: "Fork" }).click();
-    await page.getByRole("button", { name: "➕" }).click();
-    await page
-      .getByRole("row", { name: "Default 0 5 ❌" })
-      .getByRole("textbox")
-      .fill("Default123");
-    await page
-      .getByRole("cell", { name: "Default123" })
-      .getByRole("textbox")
-      .fill("New criterion");
-    await page.getByRole("cell", { name: "0" }).getByPlaceholder("0").fill("1");
-    await page
-      .getByRole("row", { name: "Default1 1 5 ❌" })
-      .getByRole("button")
-      .click();
-    await page
-      .getByRole("row", { name: "New criterion 1 5 ❌" })
-      .getByRole("slider")
-      .fill("7");
-    await page.getByRole("button", { name: "Calculate" }).click();
-    // TODO check final rating
-    await page.getByRole("button", { name: "Save" }).click();
+    await page.getByRole("link", { name: "FokedRating1" }).click();
+    await expect(page.getByRole("heading")).toContainText(
+      "Rating Name: FokedRating1"
+    );
   });
 });
 
